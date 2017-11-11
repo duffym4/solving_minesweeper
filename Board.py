b062db59feca6680d7e52f13bf8209c572c12b69
AndySos <soszya@rpi.edu>
2017-11-11 13:52:31 -0500

    Mine placement

--- Board.py
+++ Board.py
@@ -1,5 +1,17 @@
-import random
+import random as rand
 class Board(object):
-    def __init__(self,dimension):
-        Board = []
-        
+	"""
+	Internal representation of the board,
+	grid is a list of board rows, where
+	'True' represents a mine 
+	"""
+    def __init__(self,nrows,ncols,mines):
+    	# Initialize grid
+        grid = [[False]*ncols]*nrows
+        i = 0
+        while (i<mines):
+        	rowPlacement = rand.randint(0,rows-1)
+        	colPlacement = rand.randint(0,cols-1)
+        	if (!grid[rowPlacement][colPlacement]):
+        		grid[rowPlacement][colPlacement] = True
+        		i+=1
