188ee162f62f022f9e0bf1c99fc562c9a199e314
duffym4 <duffym4@rpi.edu>
2017-11-11 13:50:33 -0500

    Added get cell function

--- Board.py
+++ Board.py
@@ -1,5 +1,15 @@
 import random
+
 class Board(object):
     def __init__(self,dimension):
         Board = []
         
+
+    def getCell(self, x, y)
+    	bombs = 0
+    	for x0 in range(-1, 2):
+    		for y0 in range (-1, 2):
+    			if abs(x0) + abs(y0) == 1:
+    				if Grid[y + y0] and Grid[y + y0][x + x0]:
+    					bombs+=1
+    	return bombs
