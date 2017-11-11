8e29d5577d2aca216beca9053f614559abcdd9d8
duffym4 <duffym4@rpi.edu>
2017-11-11 15:35:12 -0500

    Image testing

--- pygletTest.py
+++ pygletTest.py
@@ -2,18 +2,23 @@ import pyglet
 
 window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello, world',
+label = pyglet.text.Label('0',
 						  font_name='Times New Roman',
 						  font_size=36,
 						  x=window.width//2, y=window.height//2,
 						  anchor_x='center', anchor_y='center')
-image = pyglet.image.load('Closed.png')
+image = pyglet.image.load('images/tiles/Closed.png')
 
 @window.event
 def on_draw():
 	window.clear()
 	label.draw()
-
 	image.blit(0, 0)
 
+
+def update(dt):
+
+	label.text = '5'
+    # ...
+pyglet.clock.schedule_interval(update, 0.1)
 pyglet.app.run()
