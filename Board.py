6150d15829a957566771339da9f75de58237311f
duffym4 <duffym4@rpi.edu>
2017-11-11 14:09:15 -0500

    Made Tabs and Main

--- Board.py
+++ Board.py
@@ -6,24 +6,23 @@ class Board(object):
 	grid is a list of board rows, where
 	'True' represents a mine 
 	"""
-    def __init__(self,nrows,ncols,mines):
-    	# Initialize grid
-        grid = [[False]*ncols]*nrows
-        i = 0
-        while (i<mines):
-        	rowPlacement = rand.randint(0,rows-1)
-        	colPlacement = rand.randint(0,cols-1)
-        	if (!grid[rowPlacement][colPlacement]):
-        		grid[rowPlacement][colPlacement] = True
-        		i+=1
+	def __init__(self,nrows,ncols,mines):
+		# Initialize grid
+		grid = [[False]*ncols]*nrows
+		i = 0
+		while (i<mines):
+			rowPlacement = rand.randint(0,rows-1)
+			colPlacement = rand.randint(0,cols-1)
+			if (!grid[rowPlacement][colPlacement]):
+				grid[rowPlacement][colPlacement] = True
+				i+=1
 
-    def getCell(self, x, y)
-    	if grid[y] and grid[y][x]:
-    		return 9
-    	bombs = 0
-    	for x0 in range(-1, 2):
-    		for y0 in range (-1, 2):
-    			if abs(x0) + abs(y0) == 1:
-    				if grid[y + y0] and grid[y + y0][x + x0]:
-    					bombs+=1
-    	return bombs
+	def getCell(self, x, y)
+   		if grid[y] and grid[y][x]:
+			return 9
+		bombs = 0
+		for x0 in range(-1, 2):
+			for y0 in range (-1, 2):
+				if grid[y + y0] and grid[y + y0][x + x0]:
+					bombs+=1
+		return bombs
