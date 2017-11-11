b9df74bb6efe8973eeeff4777894bce35463ecb8
duffym4 <duffym4@rpi.edu>
2017-11-11 14:53:23 -0500

    Fixed Board Bugs

--- Board.py
+++ Board.py
@@ -9,11 +9,17 @@ class Board(object):
 
 	def __init__(self,_nrows,_ncols,_mines):
 		# Initialize grid
-		self.grid = [[False]*_ncols]*_nrows
+
 		# Board data
 		self.nrows = _nrows
 		self.ncols = _ncols
 		self.mines = _mines
+		self.grid = []
+		for y in range(0, self.nrows):
+			self.grid.append([])
+			for x in range(0, self.ncols):
+				self.grid[y].append(False)
+
 
 		# Place the mines
 		for i in range(_mines):
@@ -32,18 +38,19 @@ class Board(object):
 					colPlacement+=1
 
 	def getCell(self, x, y):
-		if self.grid[y] and self.grid[y][x]:
+		if self.grid[y][x]:
 			return 9
 		bombs = 0
 		for x0 in range(-1, 2):
 			for y0 in range (-1, 2):
 				if (y + y0) in range (0, self.nrows) and (x + x0) in range(0, self.ncols):
-					bombs+=1
+					if self.grid[y+y0][x+x0]:
+						bombs+=1
 		return bombs
 
 	def printGrid(self):
-		for x in range(0, self.ncols):
-			for y in range(0, self.nrows):
+		for y in range(0, self.nrows):
+			for x in range(0, self.ncols):
 				if self.grid[y][x]:
 					print(1, end="")
 				else:
