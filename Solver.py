95654ced533c0357bf35ccf08086572e1bc9d2a9
Aidan Anselmi <anselmi2011@gmail.com>
2017-11-11 23:01:17 -0500

    gg

--- Solver.py
+++ Solver.py
@@ -1,5 +1,7 @@
 
 
+#count how many of an input value are in adjacent tiles to the 
+#input x and y value
 def CountValue(x, y, board, value):
 	rows = board.nrows
 	cols = board.ncols
@@ -11,11 +13,13 @@ def CountValue(x, y, board, value):
 		for j in range(y-1, y+1):
 			if(j < 0 or j > cols):
 				continue
+			#do not count yourself
 			if(i == x and j ==y):
 				continue
 			if(board.grid[i][j].value == value):
 				count += 1
-
+#if a tile has an equal number of adjacent unmarked tiles to its 
+#value flag all adjacent tiles
 def MarkFlags(x, y, board):
 	blanks = CountValue(x, y, board, -1)
 	if(board.grid[x][y].value == blanks):
@@ -30,6 +34,8 @@ def MarkFlags(x, y, board):
 				if(board.grid[i][j].value == -1)
 					board.setMarking(i, j, 2)
 
+#if a tile has an equal number of adjacent flags to its value,
+#reveal all other adjacent tiles
 def ActivateTiles(x, y, board):
 	flags = CountValue(x, y, board, 10)
 	if(board.grid[x][y].value == flags):
@@ -43,6 +49,39 @@ def ActivateTiles(x, y, board):
 					continue
 				if(board.grid[i][j].value == -1)
 					board.activate(i,j)
+
+#reduces a matrix to upper triangular form
+def UpperTriangular(matrix):
+
+  rows = len(matrix)
+  cols = len(matrix[0])
+
+  for i in range(0,min(rows, cols)):
+
+    #calculate iMax
+    iMax = i
+    max_val = matrix[i][i]
+    for j in range(i+1, rows):
+      if(abs(matrix[j][i]) > max_val):
+        max_val = abs(matrix[j][i])
+        iMax = j
+
+    #swap rows
+    for j in range(i, rows+1):
+      tmp = matrix[iMax][j]
+      matrix[iMax][j] = matrix[i][j]
+      matrix[i][j] = tmp
+
+    # Make all rows below this one 0 in current column
+    for j in range(i+1, rows):
+      c = -matrix[j][i]/matrix[i][i]
+      for k in range(i, rows+1):
+          if i == k:
+              matrix[j][k] = 0
+          else:
+              matrix[j][k] += c * matrix[i][k]
+
+  return matrix
 
 
 def binarySolve(matrix):
