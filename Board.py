234b8d99738f8bd1733f146f9d60c59f51d48423
Aidan Anselmi <anselmi2011@gmail.com>
2017-11-12 08:20:19 -0500

    more comments

--- Board.py
+++ Board.py
@@ -8,21 +8,23 @@ class Board(object):
 	"""
 
 	def __init__(self,_ncols,_nrows,_mines):
-		# Board data
 		self.nrows = _nrows
 		self.ncols = _ncols
 		self.mines = _mines
 		self.createBoard()
 
+	#create a grid to represent all of the tiles
+	#randomly place mines throughout the grid
 	def createBoard(self):
-		# Initialize grid
+		#Initialize grid as a 2d list of lists
 		self.grid = []
 		for y in range(0, self.nrows):
 			self.grid.append([])
+			#initialize all values as false
 			for x in range(0, self.ncols):
 				self.grid[y].append(False)
 
-		# Place the mines
+		#randomly place self.mines number of mines throughout the grid
 		for i in range(self.mines):
 			rowPlacement = rand.randint(0,self.nrows-1)
 			colPlacement = rand.randint(0,self.ncols-1)
@@ -40,22 +42,14 @@ class Board(object):
 						colPlacement = -1
 					colPlacement += 1
 
+	#return the number of adjacent mines to this tile or 9 if it is a mine
 	def getCell(self, x, y):
 		if self.grid[y][x]:
 			return 9
-		bombs = 0
+		mines = 0
 		for x0 in range(-1, 2):
 			for y0 in range (-1, 2):
 				if (y + y0) in range (0, self.nrows) and (x + x0) in range(0, self.ncols):
 					if self.grid[y+y0][x+x0]:
-						bombs+=1
-		return bombs
-
-	def printGrid(self):
-		for y in range(0, self.nrows):
-			for x in range(0, self.ncols):
-				if self.grid[y][x]:
-					print(1, end="")
-				else:
-					print(0, end="")
-			print()
+						mines+=1
+		return mines
